4c60ec878526615fbdbe0dd5eab14e1a30869a53
antoine.vianey <antoine.vianey@gmail.com>
2013-01-12 12:36:33 +0100

    Changement de licence

--- minimax4j/src/main/java/fr/pixelprose/minimax4j/IA.java
+++ minimax4j/src/main/java/fr/pixelprose/minimax4j/IA.java
@@ -4,23 +4,23 @@ import java.util.Collection;
 import java.util.List;
 
 /*
- *  This file is part of minimax4j.
- *  <https://github.com/avianey/minimax4j>
+ * This file is part of minimax4j.
+ * <https://github.com/avianey/minimax4j>
  *  
- *  Copyright (C) 2012 Antoine Vianey
- *  
- *  minimax4j is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Copyright (C) 2012 Antoine Vianey
+ * 
+ * minimax4j is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- *  minimax4j is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * minimax4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with minimax4j. If not, see <http://www.gnu.org/licenses/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with minimax4j. If not, see <http://www.gnu.org/licenses/lgpl.html>
  */
 
 /**
