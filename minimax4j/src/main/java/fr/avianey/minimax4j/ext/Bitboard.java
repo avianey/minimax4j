b8b2dbcf23a161a3490db6005adc7fa07f222965
Antoine Vianey <antoine.vianey@gmail.com>
2014-06-20 16:45:11 +0200

    Added a alternate way for HammingWeight

--- minimax4j/src/main/java/fr/avianey/minimax4j/ext/Bitboard.java
+++ minimax4j/src/main/java/fr/avianey/minimax4j/ext/Bitboard.java
@@ -6,20 +6,58 @@ package fr.avianey.minimax4j.ext;
  */
 public class Bitboard {
 
-    // for 64 bit numbers
-    public static long numberOfSetBits64(long i) {
+    /**
+     * Count number of bits set to one in the given long (64-bit integers).
+     * @param i
+     * @return
+     */
+    public static long numberOfSetBits64_1(long i) {
         i = i - ((i >>> 1) & 0x5555555555555555L);
         i = (i & 0x3333333333333333L) + ((i >>> 2) & 0x3333333333333333L);
         i = ((i + (i >>> 4)) & 0x0F0F0F0F0F0F0F0FL);
         return (i * (0x0101010101010101L)) >>> 56;
     }
+    
+    /**
+     * Count number of bits set to one in the given long (64-bit integers).<br/>
+     * Faster when only few bits are set to one.
+     * @param i
+     * @return
+     */
+    public static long numberOfSetBits64_2(long i) {
+        int count = 0;
+        while (i != 0) {
+        	i &= (i - 1L);
+        	count++;
+        }
+        return count;
+    }
 
-    // for 32 bit integers
+    /**
+     * Count number of bits set to one in the given int (32-bit integers).
+     * @param i
+     * @return
+     */
     public static int numberOfSetBits32(int i) {
         i = i - ((i >>> 1) & 0x55555555);
         i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
         i = ((i + (i >>> 4)) & 0x0F0F0F0F);
         return (i * (0x01010101)) >>> 24;
+    }
+
+    /**
+     * Count number of bits set to one in the given int (32-bit integers).<br/>
+     * Faster when only few bits are set to one.
+     * @param i
+     * @return
+     */
+    public static long numberOfSetBits32_2(int i) {
+        int count = 0;
+        while (i != 0) {
+        	i &= i-1;
+        	count++;
+        }
+        return count;
     }
 
 }
